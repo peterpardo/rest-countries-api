470bb899d66f5a9537be5972faf4adc860567e94
Peter Carl Pardo <peterpardo123@gmail.com>
2022-06-09 17:30:18 +0800

    Fixed dark mode logic

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,34 +1,37 @@
-import './navbar.css';
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon as farFaMoon } from '@fortawesome/free-regular-svg-icons';
-import { faMoon as fasFaMoon } from '@fortawesome/free-solid-svg-icons';
-
+import "./navbar.css";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon as farFaMoon } from "@fortawesome/free-regular-svg-icons";
+import { faMoon as fasFaMoon } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-  const [theme, setTheme] = useState("Dark Mode")
-  
+  const [theme, setTheme] = useState("Dark Mode");
+
   const handleTheme = (e) => {
-    if (theme == "Dark Mode") {
+    if (theme === "Dark Mode") {
       // set the theme to light mode
-      document.documentElement.className = 'dark';
-      setTheme('Light Mode');
+      document.documentElement.className = "dark";
+      setTheme("Light Mode");
     } else {
       // set the theme to dark mode
-      document.documentElement.className = 'light';
-      setTheme('Dark Mode');
+      document.documentElement.className = "light";
+      setTheme("Dark Mode");
     }
-  }
-  
+  };
+
   return (
     <nav>
-      <div className="navWrapper">
-        <h1 className="navTitle">Where in the world?</h1>
+      <div className='navWrapper'>
+        <h1 className='navTitle'>Where in the world?</h1>
         <div>
-          <button type="button" className="themeBtn" onClick={() => handleTheme(theme)}>
+          <button
+            type='button'
+            className='themeBtn'
+            onClick={() => handleTheme(theme)}
+          >
             {theme === "Dark Mode" ? (
               <FontAwesomeIcon icon={farFaMoon} />
-              ) : (
+            ) : (
               <FontAwesomeIcon icon={fasFaMoon} />
             )}
             <span>{theme}</span>
@@ -36,7 +39,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
